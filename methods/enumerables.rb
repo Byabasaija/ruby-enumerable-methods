e0ea3fc5477b321ee57b11a7aa787bed66f88b01
Nihal Mohammed <mnihal64@outlook.com>
2020-11-24 14:02:15 +0530

    my_each_with_index done

--- methods/enumerables.rb
+++ methods/enumerables.rb
@@ -2,12 +2,24 @@ module Enumerable
    def my_each
     return to_enum(:my_each) unless block_given?
 
-     for item in self
-      yield(item)
+     self.size.times do |index|
+      yield self[index]
      end
      self
    end
-#    arr = [4,4,5,6]
-#    arr.my_each {|item| puts item}
-     
+    arr = [4,4,5,6]
+    arr.my_each {|item| puts item}
+
+   def my_each_with_index
+      return to_enum(:my_each) unless block_given?
+
+      self.size.times do |index|
+         yield self[index], index
+      end
+      self
+   end
+
 end
+
+arr = [4,4,5,6]
+arr.my_each_with_index {|item, index| print "#{item} ==> #{index} "}
